fb3a26c4a1b101a7e2eae070cebd97a699675213
asmaakhaled01 <asmaakhaled01@gmail.com>
2017-11-29 23:24:59 +0000

    solving authentication logout bug

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -30,7 +30,8 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'getLogout']);
+        $this->middleware('guest', ['except' => ['logout', 'getLogout']]);
+
     }
 
     /**
